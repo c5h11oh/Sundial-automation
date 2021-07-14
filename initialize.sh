cfb9ae32eb6a9ddbc2e92dd2391bf4091ae8ee32
Sven Hwang <sven.chwen@gmail.com>
2021-07-14 12:36:09 -0500

    vscode debug

--- initialize.sh
+++ initialize.sh
@@ -7,3 +7,6 @@ ln -s /opt/db/Sundial ~/Sundial
 # 2. link "setup_sundial.sh" to home directory and chmod +x
 ln -s /opt/db/automation/setup_sundial.sh ~/setup_sundial.sh
 chmod +x ~/setup_sundial.sh
+
+# 3. enables vscode-debug (gdb) to attach existing process (Wuh-Chwen)
+sudo sysctl -w kernel.yama.ptrace_scope=0
